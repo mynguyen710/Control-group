28665bd7513041e0924e45db0d0a7bac78608268
mynguyen710 <58849078+mynguyen710@users.noreply.github.com>
2025-05-09 18:40:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1592,5 +1592,6 @@ def inject_defensive_checks(code: str) -> str:
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, message="Boolean Series key will be reindexed")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5009, use_reloader=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Get port from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
